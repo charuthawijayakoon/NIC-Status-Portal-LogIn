1d4de71232ad4bf7e3f5dd46129235d14043348c
Charutha Wijayakoon <charutha.w@informaticsint.com>
2023-05-09 10:00:01 +0530

    Upload the Code Structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { ImageModule } from 'primeng/image';
+import { InputSwitchModule } from 'primeng/inputswitch';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ButtonModule,
+    AvatarModule,
+    ImageModule,
+    InputSwitchModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
